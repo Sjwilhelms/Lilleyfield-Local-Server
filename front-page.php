416262788b50e24aaa1333cf73a5a44da58207bb
Sam <sjwilliams388@gmail.com>
2025-02-13 15:37:12 +0000

    add to front-page

--- front-page.php
+++ front-page.php
@@ -5,6 +5,12 @@
         <div class="hero-image">
             <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/cuttingboard.jpg" alt="">
         </div>
+        <div class="social-cta">
+            <a href="">
+                <p><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/instagram-brands-solid.svg">
+                </p>
+            </a>
+        </div>
         <div class="about-lilleyfield">
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae repellat quam officia tempore suscipit illum aspernatur, sint eius amet harum excepturi quae commodi, maiores corporis aliquid dolores cupiditate quibusdam voluptatum.</p>
         </div>
