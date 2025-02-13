e790f7e68ea5d7bf1ca80d6d8a6a726297a57d7c
Sam <sjwilliams388@gmail.com>
2025-02-13 14:07:36 +0000

    add front-page.php

---
+++ front-page.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<main>
+    <div class="hero-container">
+        <div class="hero-image">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/cuttingboard.jpg" alt="">
+        </div>
+        <div class="about-lilleyfield">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae repellat quam officia tempore suscipit illum aspernatur, sint eius amet harum excepturi quae commodi, maiores corporis aliquid dolores cupiditate quibusdam voluptatum.</p>
+        </div>
+    </div>
+
+
+</main>
+
+<?php get_footer(); ?>
