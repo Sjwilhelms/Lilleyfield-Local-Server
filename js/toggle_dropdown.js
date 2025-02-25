a23a34e6c2827f572ed74d0e2b5c3144cff37a9f
Sam <sjwilliams388@gmail.com>
2025-02-25 11:29:45 +0000

    toggle menu function js WIP

--- js/toggle_dropdown.js
+++ js/toggle_dropdown.js
@@ -1,14 +1,24 @@
 const toggleButton = document.querySelector('.navbar-toggler');
-const navbarMenu = document.querySelector('.navbar-list-flex-mobile');
+const navbarMenu = document.querySelector('.header-navigation-wrapper');
 
 // navbar toggler
 
+function checkWidth() {
+    if (window.innerWidth <= 1000) {
+        // Below breakpoint - make sure menu is hidden by default
+        navbarMenu.classList.add('hidden');
+    } else {
+        // Above breakpoint - make sure menu is visible
+        navbarMenu.classList.remove('hidden');
+    }
+}
+
 toggleButton.addEventListener('click', () => {
-    navbarMenu.classList.toggle('active');
+    navbarMenu.classList.toggle('hidden');
 });
 
 window.addEventListener('resize', () => {
-    if (navbarMenu.classList.contains('active')) {
-        navbarMenu.classList.remove('active');
+    if (navbarMenu.classList.contains('hidden')) {
+        navbarMenu.classList.remove('hidden');
     }
 });
