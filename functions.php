1f3c5149cfda6e7b2062f45dad9d00cb99d22c02
Sam <sjwilliams388@gmail.com>
2025-03-03 15:28:42 +0000

    add to mobile style

--- functions.php
+++ functions.php
@@ -12,3 +12,42 @@ function child_theme_enqueue_styles()
     );
 }
 add_action('wp_enqueue_scripts', 'child_theme_enqueue_styles');
+
+
+/**
+ * Redirect Storefront custom logo to shop page
+ */
+function storefront_child_custom_logo_link($html)
+{
+    $shop_url = get_permalink(wc_get_page_id('shop'));
+
+    $html = str_replace(home_url(), $shop_url, $html);
+
+    return $html;
+}
+add_filter('get_custom_logo', 'storefront_child_custom_logo_link');
+
+
+/**
+ * Remove WooCommerce breadcrumbs in Storefront theme
+ */
+function remove_storefront_woocommerce_breadcrumbs()
+{
+    remove_action('storefront_before_content', 'woocommerce_breadcrumb', 10);
+}
+add_action('init', 'remove_storefront_woocommerce_breadcrumbs');
+
+function custom_remove_account_menu_items($menu_links)
+{
+
+    unset($menu_links['dashboard']);
+    // unset( $menu_links['orders'] );     
+    unset($menu_links['downloads']);
+    // unset( $menu_links['edit-address'] );  
+    // unset( $menu_links['payment-methods'] ); 
+    // unset( $menu_links['edit-account'] );  
+    // unset( $menu_links['customer-logout'] );
+
+    return $menu_links;
+}
+add_filter('woocommerce_account_menu_items', 'custom_remove_account_menu_items');
