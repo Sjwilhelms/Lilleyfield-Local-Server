16777e42b20c9a0ea43d3ec586a4a3a14f8cf08a
Sam <sjwilliams388@gmail.com>
2025-03-10 12:30:05 +0000

    add preload to functions.php

--- functions.php
+++ functions.php
@@ -13,10 +13,8 @@ function child_theme_enqueue_styles()
 }
 add_action('wp_enqueue_scripts', 'child_theme_enqueue_styles');
 
+// Redirect Storefront custom logo to shop page
 
-/**
- * Redirect Storefront custom logo to shop page
- */
 function storefront_child_custom_logo_link($html)
 {
     $shop_url = get_permalink(wc_get_page_id('shop'));
@@ -27,15 +25,15 @@ function storefront_child_custom_logo_link($html)
 }
 add_filter('get_custom_logo', 'storefront_child_custom_logo_link');
 
+//   Remove WooCommerce breadcrumbs in Storefront theme
 
-/**
- * Remove WooCommerce breadcrumbs in Storefront theme
- */
 function remove_storefront_woocommerce_breadcrumbs()
 {
     remove_action('storefront_before_content', 'woocommerce_breadcrumb', 10);
 }
 add_action('init', 'remove_storefront_woocommerce_breadcrumbs');
+
+// remove menu account menu links
 
 function custom_remove_account_menu_items($menu_links)
 {
@@ -51,3 +49,12 @@ function custom_remove_account_menu_items($menu_links)
     return $menu_links;
 }
 add_filter('woocommerce_account_menu_items', 'custom_remove_account_menu_items');
+
+//  preload both variations of logo images
+
+function custom_preload_logo_images()
+{
+    echo '<link rel="preload" href="' . esc_url(get_stylesheet_directory_uri() . '/wp-content/uploads/2025/02/logo_inline.png') . '" as="image">';
+    echo '<link rel="preload" href="' . esc_url(get_stylesheet_directory_uri() . '/wp-content/uploads/2025/02/lilleyfield_text.png') . '" as="image">';
+}
+add_action('wp_head', 'custom_preload_logo_images', 1);
